a465a1d0cb8308859acab0194da8c1f3eccfeb75
Clayton Moore <ccmoore0121@gmail.com>
2024-03-25 21:42:48 -0500

    End Example Code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import ContactList from "./ContactList";
+import "./App.css";
+import ContactForm from "./ContactForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentContact, setCurrentContact] = useState({})
+
+  useEffect(() => {
+    fetchContacts()
+  }, []);
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setCurrentContact({})
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
+  const openEditModal = (contact) => {
+    if (isModalOpen) return
+    setCurrentContact(contact)
+    setIsModalOpen(true)
+  }
+
+  const onUpdate = () => {
+    closeModal()
+    fetchContacts()
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <ContactList contacts={contacts} updateContact={openEditModal} updateCallback={onUpdate} />
+      <button onClick={openCreateModal}>Create New Contact</button>
+      {isModalOpen && <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={closeModal}>&times;</span>
+          <ContactForm existingContact={currentContact} updateCallback={onUpdate} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      }
     </>
-  )
+  );
 }
 
-export default App
+export default App;
