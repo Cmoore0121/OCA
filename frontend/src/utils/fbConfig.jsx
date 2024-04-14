499b6d0f7ef2efd18bdfec8779d6b43db46b0c6a
Berk Yalcinkaya <berkyalcinkaya55@gmail.com>
2024-04-14 14:59:19 -0700

    firebase ipynb

---
+++ frontend/src/utils/fbConfig.jsx
@@ -0,0 +1,14 @@
+import { initializeApp } from 'fireabse/app'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCf8iDTkKmyen5vmq8b9bo8zEU5hXjGfqc",
+    authDomain: "oca-sourcing.firebaseapp.com",
+    projectId: "oca-sourcing",
+    storageBucket: "oca-sourcing.appspot.com",
+    messagingSenderId: "884281157514",
+    appId: "1:884281157514:web:d93d3b87d6b4a5cfa90a73",
+    measurementId: "G-MYNZ2CKXQ4"
+  };
+
+  initializeApp(firebaseConfig);
